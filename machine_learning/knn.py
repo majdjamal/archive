46fc07fdbdf0839b1b7d0320d385533ea0e869d1
Majd Jamal <majdj@kth.se>
2021-03-20 19:03:22 +0100

    comments on knn

--- machine_learning/knn.py
+++ machine_learning/knn.py
@@ -12,7 +12,7 @@ class kNN:
 	"""
 	def __init__(self, k_neigh = 10):
 
-		self.k_neigh = k_neigh	
+		self.k_neigh = k_neigh
 		self.patterns = None
 		self.targets = None
 
@@ -27,15 +27,15 @@ class kNN:
 		return score
 
 	def fit(self, X, y):
-		""" Adjusts patterns and targets. 
-		:param X: patterns
-		:param y: patterns
+		""" Adjusts patterns and targets.
+		:param X: Training data (Shape = (Npts, Ndim))
+		:param y: Training labels
 		"""
 		self.patterns = X
 		self.targets = y
 
 	def predict(self, X):
-		""" Predicts labels of data points.  
+		""" Predicts labels of data points.
 		:param X: Data points (Shape = (Npts, Ndim))
 		:return predictions: predicted labels
 		"""
@@ -46,7 +46,7 @@ class kNN:
 		D = distance_matrix(X, self.patterns)
 
 		for i in range(N):
-			
+
 			neighbors = D[i].argsort()[:self.k_neigh]
 			neighbor_labels = self.targets[neighbors]
 
